8d4a299f85e6c47db89e87b6a709a629b785e2e1
pharzan <farzan.tinati@gmail.com>
2018-10-02 17:25:11 +0330

    added font metrics calculation

--- imager_creator.py
+++ imager_creator.py
@@ -12,55 +12,53 @@ with open("code.txt", "w") as text_file:
     text_file.write("{0}".format(code))
 
 shell_cmd = """ vim -c "argdo setf javascript | execute 'normal! gg=G' | execute 'call JsBeautify()' | execute 'wq!' | update" code.txt """
-p = subprocess.Popen(shell_cmd, shell=True, stdout=subprocess.PIPE)
+p = subprocess.call(shell_cmd, shell=True, stdout=subprocess.PIPE)
+with open('code.txt', 'r') as myfile:
+    code = myfile.read()
 
 with Drawing() as ctx:
-    with Drawing() as draw:
-        draw.stroke_color = Color('black')
-        draw.stroke_width = 2
-        draw.fill_color = Color('black')
+    
 
-        EDITOR_WIDTH = 1000-80
-        EDITOR_HEIGHT = 1000-80
+        with Image(width=1080, height=1080, background=Color("WHITE")) as img:
+            with Drawing() as draw:
+                draw.stroke_color = Color('black')
+                draw.stroke_width = 2
+                draw.fill_color = Color('black')
 
-        EDITOR_LEFT = 80
-        EDITOR_TOP = 80
-        draw.fill_color = Color('white')
+                EDITOR_WIDTH = 1000-80
+                EDITOR_HEIGHT = 1000-80
 
-        draw.rectangle(left=EDITOR_LEFT+10, top=EDITOR_TOP+10, right=None, bottom=None, width=EDITOR_WIDTH,
-                       height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
+                EDITOR_LEFT = 80
+                EDITOR_TOP = 80
+                draw.fill_color = Color('white')
 
-        draw.fill_color = Color('black')
+                draw.rectangle(left=EDITOR_LEFT+10, top=EDITOR_TOP+10, right=None, bottom=None, width=EDITOR_WIDTH,
+                            height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
 
-        draw.rectangle(left=EDITOR_LEFT, top=EDITOR_TOP, right=None, bottom=None, width=EDITOR_WIDTH,
-                       height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
+                draw.fill_color = Color('black')
 
-        
+                draw.rectangle(left=EDITOR_LEFT, top=EDITOR_TOP, right=None, bottom=None, width=EDITOR_WIDTH,
+                            height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
 
-        draw.fill_color = Color('red')
-        draw.circle((100, 100),  # Center point
-                    (105, 105))  # Perimeter point
+                draw.fill_color = Color('red')
+                draw.circle((100, 100),  # Center point
+                            (105, 105))  # Perimeter point
 
-        draw.fill_color = Color('yellow')
-        draw.circle((120, 100),  # Center point
-                    (125, 105))  # Perimeter point
+                draw.fill_color = Color('yellow')
+                draw.circle((120, 100),  # Center point
+                            (125, 105))  # Perimeter point
 
-        draw.fill_color = Color('green')
-        draw.circle((140, 100),  # Center point
-                    (145, 105))  # Perimeter point
+                draw.fill_color = Color('green')
+                draw.circle((140, 100),  # Center point
+                            (145, 105))  # Perimeter point
 
-        draw.fill_color = Color('white')
-        draw.stroke_color = Color('transparent')
-        draw.font=''
-        draw.text(x=150,
-                  y=175,
-                  body=code)
-
-        with nested(Image(width=1080, height=1080, background=Color("WHITE")),
-                    Image(width=100, height=100, background=Color("transparent"))) as (img,
-                                                                                       shadow):
-
-            draw(img)
-            img.sample(1080, 1080)
-            img.save(filename="output.png")
-
+                draw.fill_color = Color('white')
+                draw.stroke_color = Color('transparent')
+                draw.font_family = 'monospace'
+                draw.font_size = 36
+                metrics = draw.get_font_metrics(img,code, multiline=False)
+                print(metrics)
+                draw.text(x=150, y=175, body=code)
+                draw(img)
+                img.sample(1080, 1080)
+                img.save(filename="output.png")

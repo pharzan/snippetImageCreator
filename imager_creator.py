a161d86071b5b4f060c6f504de86c1a71964ff69
pharzan <farzan.tinati@gmail.com>
2018-10-02 19:28:06 +0330

    added draw text with max width and line spacing

--- imager_creator.py
+++ imager_creator.py
@@ -16,9 +16,24 @@ p = subprocess.call(shell_cmd, shell=True, stdout=subprocess.PIPE)
 with open('code.txt', 'r') as myfile:
     code = myfile.read()
 
-with Drawing() as ctx:
-    
 
+def get_words(para):
+    words = para.split(" ")
+    lines = []
+    with Image(height=1080, width=1080) as img:
+        line = words.pop(0)
+        for word in words:
+            line_width = draw.get_font_metrics(img, line + " " + word).text_width
+            if line_width < 370:
+                line = line + " " + word
+            else:
+                lines.append(line)
+                line = word
+        if line != "":
+            lines.append(line)
+    return lines
+
+with Drawing() as ctx:
         with Image(width=1080, height=1080, background=Color("WHITE")) as img:
             with Drawing() as draw:
                 draw.stroke_color = Color('black')
@@ -32,8 +47,7 @@ with Drawing() as ctx:
                 EDITOR_TOP = 80
                 draw.fill_color = Color('white')
 
-                draw.rectangle(left=EDITOR_LEFT+10, top=EDITOR_TOP+10, right=None, bottom=None, width=EDITOR_WIDTH,
-                            height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
+                draw.rectangle(left=EDITOR_LEFT+10, top=EDITOR_TOP+10, right=None, bottom=None, width=EDITOR_WIDTH, height=EDITOR_HEIGHT, radius=5, xradius=None, yradius=None)
 
                 draw.fill_color = Color('black')
 
@@ -51,14 +65,26 @@ with Drawing() as ctx:
                 draw.fill_color = Color('green')
                 draw.circle((140, 100),  # Center point
                             (145, 105))  # Perimeter point
-
+                a = get_words(code)
+                # print(a)
                 draw.fill_color = Color('white')
                 draw.stroke_color = Color('transparent')
-                draw.font_family = 'monospace'
-                draw.font_size = 36
-                metrics = draw.get_font_metrics(img,code, multiline=False)
+                draw.font_family = './Raleway-Thin.ttf'
+                draw.font_size = 24
+                draw.text_interline_spacing = 15
+                # img.caption(left=150, top=200,
+                #             font='./Raleway-Regular.ttf', text="hello")
+
+                # print(''.join([str(x) for x in a]))
+                txt = '\n'.join([str(x) for x in a])
+                draw.text(x=175, y=120,body=txt)
+                metrics = draw.get_font_metrics(img, txt, multiline=True)
                 print(metrics)
-                draw.text(x=150, y=175, body=code)
+                # for i,line in enumerate(a):
+                #     metrics = draw.get_font_metrics(img, line, multiline=True)
+                #     draw.text(x=150, y=120+(i*35)+int(metrics.text_height), body=line)
+                    # print( line, '\n')
                 draw(img)
                 img.sample(1080, 1080)
                 img.save(filename="output.png")
+
